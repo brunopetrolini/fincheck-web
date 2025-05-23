0c4bff3100d4490f40eef3e1206aa03e37b6d8c0
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-05-22 22:13:29 -0300

    feat: configure Tailwind CSS with custom theme and animations

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
