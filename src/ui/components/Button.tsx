92bb50221df360602f08e658639553e1e4a471be
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-05-26 20:52:08 -0300

    feat: enhance Button component to accept children and update Register page layout

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,15 +1,17 @@
 import type { ComponentProps } from 'react';
 
-type ButtonProps = ComponentProps<'button'>;
+type ButtonProps = ComponentProps<'button'> & {
+  children: React.ReactNode;
+};
 
-export function Button(props: ButtonProps) {
+export function Button({ className, children, ...props }: ButtonProps) {
   return (
     <button
       type="submit"
-      className="h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400"
+      className={`h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400 ${className ?? ''}`}
       {...props}
     >
-      Entrar
+      {children}
     </button>
   );
 }
--- src/ui/pages/Register/index.tsx
+++ src/ui/pages/Register/index.tsx
@@ -1,3 +1,31 @@
+import { Link } from 'react-router-dom';
+
+import { Button } from '@/ui/components/Button';
+import { Input } from '@/ui/components/Input';
+
 export function Register() {
-  return <h1>Register Page</h1>;
+  return (
+    <div className="flex flex-col gap-16">
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold tracking-[-1px] text-gray-900">Crie sua conta</h1>
+
+        <p className="space-x-2 tracking-[-0.5px]">
+          <span className="text-gray-700">Já possui uma conta?</span>
+          <Link to="/login" className="font-medium text-teal-900">
+            Fazer login
+          </Link>
+        </p>
+      </header>
+
+      <form className="flex flex-col gap-4">
+        <Input name="name" type="text" placeholder="Nome" autoCapitalize="words" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </div>
+  );
 }
