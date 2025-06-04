73beffe2f4a98b99e17d956c136df1e319ea78d5
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-06-03 21:13:21 -0300

    feat: add Spinner component and integrate loading state in Button component

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -2,6 +2,8 @@ import type { ComponentProps } from 'react';
 
 import { cn } from '@/utils/cn';
 
+import { Spinner } from './Spinner';
+
 type ButtonProps = ComponentProps<'button'> & {
   children: React.ReactNode;
   isLoading?: boolean;
@@ -11,14 +13,14 @@ export function Button({ className, children, isLoading = false, disabled, ...pr
   return (
     <button
       type="submit"
-      disabled={isLoading || disabled}
+      disabled={disabled || isLoading}
       className={cn(
-        'h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400',
+        'flex h-12 items-center justify-center rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400',
         className,
       )}
       {...props}
     >
-      {children}
+      {isLoading ? <Spinner className="h-6 w-6" /> : children}
     </button>
   );
 }
